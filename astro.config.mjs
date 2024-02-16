5a2c3dd368782336686d097171bfe5d86033114e
Yarce22 <alejomira96@gmail.com>
2024-02-16 00:24:29 -0500

    Se corrige el deploy de las imagenes. Intento3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Yarce22.github.io',
-  buildOptions: {
-    baseUrl: '/Yarce22',
-  },
 });

78ea74af6900f12e6301abe5a3a7427741874bd7
Yarce22 <alejomira96@gmail.com>
2024-02-23 15:21:12 -0500

    Cambio en el tipo de input email

--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,11 +1,11 @@
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_BMa7ow_s.mjs';
+import { manifest } from './manifest_B6h9m6Zw.mjs';
 import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./chunks/generic_CbnVaQ7d.mjs');
-const _page1 = () => import('./chunks/thanks_btyCzuJq.mjs');
-const _page2 = () => import('./chunks/index_CKxFAftZ.mjs');
+const _page1 = () => import('./chunks/thanks_jtxTfbzd.mjs');
+const _page2 = () => import('./chunks/index_BBR-eDQu.mjs');
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/thanks.astro", _page1],
@@ -18,7 +18,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "906af231-27c6-4e0c-85c0-e574874eb6bd"
+    "middlewareSecret": "f17b84b3-4e6e-42ae-8a79-b90ad577425b"
 };
 const _exports = serverEntrypointModule.createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
